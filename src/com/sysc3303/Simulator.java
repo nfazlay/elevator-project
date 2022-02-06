ba2fd1efd2c04cd4826fb6aa67f49dac8e4fb7b4
Fazlay Rabbi <fazlayrabbi@cmail.carleton.ca>
2022-02-05 23:03:11 -0500

    Add Simulator to run Simulator

---
+++ src/com/sysc3303/Simulator.java
@@ -0,0 +1,20 @@
+package com.sysc3303;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Simulator {
+
+	public static void main(String[] args) throws SocketException, UnknownHostException {
+		System.out.println(String.format("----- Simulating Elevator System -----"));
+		final Scheduler server = new Scheduler(8080);
+		final Elevator elevator = new Elevator();
+		final Floor floor = new Floor();
+		
+		new Thread(server).start();
+		new Thread(elevator).start();
+		new Thread(floor).start();
+		
+	}
+
+}
