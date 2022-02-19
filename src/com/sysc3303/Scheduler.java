3a6c90de28c4bbc0810cc3c89c59b9bba12f421a
Fazlay Rabbi <fazlayrabbi@cmail.carleton.ca>
2022-02-19 16:45:27 -0500

    Add states to Scheduler

--- src/com/sysc3303/Scheduler.java
+++ src/com/sysc3303/Scheduler.java
@@ -1,12 +1,16 @@
 package com.sysc3303;
 
 import java.io.*;
+
 import java.net.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import com.sysc3303.properties.Data;
+import com.sysc3303.properties.Helper;
+import com.sysc3303.properties.StateMessage;
 import com.sysc3303.properties.Message;
+import com.sysc3303.properties.OkMessage;
 import com.sysc3303.properties.Systems;
  
 /**
@@ -17,10 +21,10 @@ import com.sysc3303.properties.Systems;
  */
 public class Scheduler implements Runnable {
     private DatagramSocket socket;
-    private DatagramPacket sendPacket, receivePacket;
-    protected Message message;
+    private DatagramPacket receivePacket;
+    protected Data message, done;
     private Queue<DatagramPacket> floorPackets = new LinkedList<DatagramPacket>();
-    private Queue<DatagramPacket> elevPackets = new LinkedList<DatagramPacket>();
+    //private Queue<DatagramPacket> elevPackets = new LinkedList<DatagramPacket>();
     
     public Scheduler(int port) throws SocketException, UnknownHostException {
         socket = new DatagramSocket(port);
@@ -32,87 +36,92 @@ public class Scheduler implements Runnable {
 		try {
 			s = new Scheduler(8080);
 			s.start();
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
     }
     
-    private void start() throws IOException{
+    @SuppressWarnings("incomplete-switch")
+	private void start() throws IOException, ClassNotFoundException{
     	try {
     		while (true) {
     			
-    			System.out.println("Scheduler: Waiting for packets...\n");
+    			System.out.println("SCHEDULER: Waiting for packet...\n");
     			byte[] bufReceived = new byte[1024];
     			receivePacket = new DatagramPacket(bufReceived, bufReceived.length);
     			socket.receive(receivePacket);
     			
-    			// For iteration 1, received data is a Passenger event that was sent initially.
-    			// This will be changed in future iterations.
     			try {
-    				message = (Message) Data.fromByteArray(receivePacket.getData());
+    				message = (Data) Data.fromByteArray(receivePacket.getData());
     			} catch (ClassNotFoundException ce) {
     				ce.printStackTrace();
     			}
     			
-    			if(message.getMessage() != "") {//Elevator sent first message
-    				//System.out.println(message);
-    				elevPackets.add(receivePacket);
-    				System.out.println("Scheduler: Elevator connected\n");
-    			}
-    			else {
-    				if (message.getSender() == Systems.FLOOR) {
-    					floorPackets.add(receivePacket);
-    					System.out.println("Scheduler: Packet received from Floor");
-    					System.out.println("Scheduler packet data: " + message);
-    					//pack message to send to Elevator
-    					message.setReceiver(Systems.ELEVATOR);
-    					message.setSender(Systems.SCHEDULER);
-    					System.out.println("Scheduler: Sending packet to Elevator");
-    					byte[] bufSend = Data.toByteArray(message);
-    					sendPacket = new DatagramPacket(bufSend, bufSend.length, 
-    							elevPackets.peek().getAddress(), elevPackets.peek().getPort());
-    					
-    					socket.send(sendPacket);
-    					System.out.println("Scheduler: Packet sent to Elevator.\n");
-    					
-    					
-    				}
-    				else{//Elevator
-    					System.out.println("Scheduler: Packet received from Elevator");
-    					System.out.println("Scheduler packet data: " + message);
-    					//pack message to send to Elevator
-    					message.setReceiver(Systems.FLOOR);
-    					message.setSender(Systems.SCHEDULER);
-    					
-    					System.out.println("Scheduler: Sending packet to Floor");
-    					byte[] bufSend = Data.toByteArray(message);
-    					sendPacket = new DatagramPacket(bufSend, bufSend.length, 
-    							floorPackets.peek().getAddress(), floorPackets.peek().getPort());
-    					
-    					socket.send(sendPacket);
-    					
-    					System.out.println("Scheduler: Packet sent to Floor.\n");
-    				}
-    			}
+    			System.out.println("SCHEDULER: Received packet from: " + message.getSender());
+    			System.out.println("SCHEDULER: Packet data: " + message);
     			
+    			switch(message.getSender()) {
+					case ELEVATOR:
+						switch(((StateMessage) message).getState()){
+							case STATIONARY:
+								//give task
+								if(!floorPackets.isEmpty()) {
+									//add more logic when more elevators
+									Message req = (Message) Data.fromByteArray(floorPackets.poll().getData());
+									sendToSystem(req, Systems.ELEVATOR, receivePacket);
+									
+									//send to floor to switch lamp off
+//									done  = new OkMessage("Turn Off Lamp");
+//									sendToSystem(done, Systems.FLOOR, floorPackets.poll());
+								}
+								else {// no packets
+									done  = new OkMessage("Cool");
+									sendToSystem(done, Systems.ELEVATOR, receivePacket);
+								}
+								break;
+							case MOVING:
+								//add logic to see if going the same direction and pick up passenger
+								//if going the same direction and above/below the pickup floor
+								//pickup the passenger respectedly
+								done  = new OkMessage("Cool");
+								sendToSystem(done, Systems.ELEVATOR, receivePacket);
+								break;
+							case OPENDOOR:
+								done  = new OkMessage("Cool");
+								sendToSystem(done, Systems.ELEVATOR, receivePacket);
+								break;
+							case CLOSEDOOR:
+								// in future: if request
+									//remove from floorreq
+									//send to floor
+								done  = new OkMessage("Cool");
+								sendToSystem(done, Systems.ELEVATOR, receivePacket);
+								break;
+						}						
+						break;
+					case FLOOR:
+						floorPackets.add(receivePacket);
+						break;
+				}
     		}
     	}
     	catch (SocketTimeoutException ex) {
-            System.out.println("Closing Scheduler socket due to inactivity");
+            System.out.println("SCHEDULER: Closing Scheduler socket due to inactivity");
             socket.close();
     	}
         	
+    }
+    
+    public void sendToSystem(Data message, Systems system, DatagramPacket packet) throws IOException {
+    	DatagramPacket sendPacket = Helper.sendPacket(message, Systems.SCHEDULER, system, packet.getAddress(), packet.getPort());
+    	socket.send(sendPacket);
     }
     
     @Override
     public void run() {
         try {
 			start();
-		} catch (IOException e) {
+		} catch (IOException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
